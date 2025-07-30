7d8007913ffecbddb7d76d3c74e7aac4a6a362e3
Begüm Akyol <99885864+begumakyol@users.noreply.github.com>
2025-07-30 10:45:27 +0300

    Delete Models/Results directory

--- Models/Results/UserLoginResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VisitorManagementSystem.Models.Results
-{
-    public class UserLoginResult
-    {
-        public bool IsLogin { get; set; }
-        public string UserName { get; set; }
-    }
-}
